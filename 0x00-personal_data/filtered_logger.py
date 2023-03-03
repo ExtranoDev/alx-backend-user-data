564ef542d1f73bb838d7c11eeea09d3738cd70e5
ExtranoDev <jeomade19@gmail.com>
2023-03-03 03:12:06 +0000

    connect to a secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,18 +3,10 @@
 import re
 import logging
 from typing import List
+import os
+import mysql.connector
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 """ list of fields that can are considered as “important” PIIs """
-
-
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """returns the log message obfuscated
-        Uses a regex to replace occurrences of certain field values"""
-    for field in fields:
-        message = re.sub(fr"{field}=.+?{separator}",
-                         f"{field}={redaction}{separator}", message)
-    return message
 
 
 class RedactingFormatter(logging.Formatter):
@@ -35,6 +27,16 @@ class RedactingFormatter(logging.Formatter):
                             super().format(record), self.SEPARATOR)
 
 
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """returns the log message obfuscated
+        Uses a regex to replace occurrences of certain field values"""
+    for field in fields:
+        message = re.sub(fr"{field}=.+?{separator}",
+                         f"{field}={redaction}{separator}", message)
+    return message
+
+
 def get_logger() -> logging.Logger:
     """ create logger object, formatter and handler """
     logger = logging.getLogger("user_data")
@@ -44,3 +46,12 @@ def get_logger() -> logging.Logger:
     handler.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.addHandler(handler)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ using databse details returns a connector to the database """
+    return mysql.connector.connect(
+                host=os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost'),
+                database=os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root'),
+                user=os.environ.get('PERSONAL_DATA_DB_NAME'),
+                password=os.environ.get('PERSONAL_DATA_DB_PASSWORD', ''))
