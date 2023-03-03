68daca2b8af7bd97880d2cd55be3139bec3a218b
ExtranoDev <jeomade19@gmail.com>
2023-03-03 23:50:55 +0000

    Checks if password is valid

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,7 +6,7 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """expects one string argument name password
     returns a salted, hashed password, which is a byte string"""
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_pass: bytes, password: str) -> bool:
@@ -15,4 +15,4 @@ def is_valid(hashed_pass: bytes, password: str) -> bool:
         hashed_password: bytes type
         password: string type
     """
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_pass)
+    return bcrypt.checkpw(bytes(password, 'utf-8'), hashed_pass)
