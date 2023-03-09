d700e3f11632edc4ff2fa2522f8d8b369978fb3f
ExtranoDev <jeomade19@gmail.com>
2023-03-09 20:25:43 +0000

    require paths with star

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,10 +10,9 @@ class Auth:
         """collects authentication path"""
         if path is None or excluded_paths in (None, []):
             return True
-        if path[-1] != '/':
-            path += '/'
-        if path in excluded_paths:
-            return False
+        for ex_path in excluded_paths:
+            if path in ex_path:
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
