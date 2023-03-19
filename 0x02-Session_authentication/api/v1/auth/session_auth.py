7cc26d61b6f3b4be100b7d16997b750025120049
ExtranoDev <jeomade19@gmail.com>
2023-03-19 22:53:49 +0000

    Updated

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Session definition"""
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -20,4 +21,12 @@ class SessionAuth(Auth):
         """retrieves a User ID based on a Session ID"""
         if session_id and type(session_id) is str:
             return SessionAuth.user_id_by_session_id.get(session_id)
+        return None
+
+    def current_user(self, request=None):
+        """retrieves a User instance based on a cookie value"""
+        if request:
+            session_user = self.session_cookie(request)
+            user_id = self.user_id_for_session_id(session_user)
+            return User.get(user_id)
         return None
