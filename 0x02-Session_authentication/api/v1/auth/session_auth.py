f8eb9e5fbeafb88c452bb9334c7e52ae06ab4eab
ExtranoDev <jeomade19@gmail.com>
2023-03-19 22:17:34 +0000

    add retriving user ID function using session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,6 +12,12 @@ class SessionAuth(Auth):
         """creates a Session ID and return it"""
         if user_id and type(user_id) is str:
             sess_id = str(uuid.uuid4())
-            self.user_id_by_session_id[sess_id] = user_id
+            SessionAuth.user_id_by_session_id[sess_id] = user_id
             return sess_id
+        return None
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """retrieves a User ID based on a Session ID"""
+        if session_id and type(session_id) is str:
+            return SessionAuth.user_id_by_session_id.get(session_id)
         return None
