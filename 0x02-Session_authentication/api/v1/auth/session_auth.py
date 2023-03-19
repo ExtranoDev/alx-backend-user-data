fdb78579b92c7d4404142274fa26cfe8cb6f2429
ExtranoDev <jeomade19@gmail.com>
2023-03-19 22:11:50 +0000

    using unique id for each session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """Session definition"""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """define session attributes and methods"""
-    pass
+    user_id_by_session_id = dict()
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID and return it"""
+        if user_id and type(user_id) is str:
+            sess_id = str(uuid.uuid4())
+            self.user_id_by_session_id[sess_id] = user_id
+            return sess_id
+        return None
