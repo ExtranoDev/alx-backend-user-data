164eba72b00125ae2a217cc6cc9efbe22699462e
ExtranoDev <jeomade19@gmail.com>
2023-05-05 22:28:06 +0000

    End to end integration test

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,17 +1,66 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-from auth import Auth
+"""End-to-end integration test"""
+import request
 
-email = 'bob@bob.com'
-password = 'MyPwdOfBob'
-auth = Auth()
 
-auth.register_user(email, password)
+def register_user(email: str, password: str) -> None:
+    """"""
+    user = {"email": email, "password": password}
+    res = requests.post('http://localhost:5000/users', data=user)
+    if res.status_code != 200:
+        assert res.json() == {"message": "email already registered"}
+    else:
+        assert res.status_code == 200
+        assert res.json() == {"email": f"{email}", "message": "user created"}
 
-print(auth.valid_login(email, password))
 
-print(auth.valid_login(email, "WrongPwd"))
+def log_in_wrong_password(email: str, password: str) -> None:
+    """"""
+    pass
 
-print(auth.valid_login("unknown@email", password))
+
+def log_in(email: str, password: str) -> str:
+    """"""
+    pass
+
+
+def profile_unlogged() -> None:
+    """"""
+    pass
+
+
+def profile_logged(session_id: str) -> None:
+    """"""
+    pass
+
+
+def log_out(session_id: str) -> None:
+    """"""
+    pass
+
+
+def reset_password_token(email: str) -> str:
+    """"""
+    pass
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """"""
+    pass
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+    register_user(EMAIL, PASSWD)
+    # log_in_wrong_password(EMAIL, NEW_PASSWD)
+    # profile_unlogged()
+    # session_id = log_in(EMAIL, PASSWD)
+    # profile_logged(session_id)
+    # log_out(session_id)
+    # reset_token = reset_password_token(EMAIL)
+    # update_password(EMAIL, reset_token, NEW_PASSWD)
+    # log_in(EMAIL, NEW_PASSWD)
