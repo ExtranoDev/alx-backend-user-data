56f60ecca066b84cf8029fad86656e9ca7e43451
ExtranoDev <jeomade19@gmail.com>
2023-05-05 21:01:14 +0000

    Add user registration to flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python3
 """a basic Flask app"""
 from flask import Flask, jsonify
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', strict_slashes=False)
 def home():
     """Landing page for app"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/adduser', methods=['POST'], strict_slashes=False)
+def adduser():
+    """create user"""
+    email = request.form['email']
+    password = request.form['email']
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 4001
 
 
 if __name__ == "__main__":
