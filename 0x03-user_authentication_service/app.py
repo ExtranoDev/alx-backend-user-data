010e3cca79fd784182e5042ce16621d1fbdd3d20
ExtranoDev <jeomade19@gmail.com>
2023-05-05 21:57:48 +0000

    logout and get user profile functions added

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -42,5 +42,30 @@ def login():
         abort(401)
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """Logout user"""
+    session_id = request.cookies.get('session_id')
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if not user:
+            abort(403)
+        AUTH.destroy_session(user.id)
+        return redirect(url_for('welcome'))
+    abort(403)
+
+
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """Get user profile
+    """
+    session_id = request.cookies.get('session_id')
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": f"{user.email}"})
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
