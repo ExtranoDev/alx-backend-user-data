d4c300df1581b0e986c9ee784c5af1b9ea107255
ExtranoDev <jeomade19@gmail.com>
2023-05-05 20:41:27 +0000

    Added support for authentication

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 Hash User password
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,3 +13,23 @@ def _hash_password(password: str) -> bytes:
     """
     password = password.encode('utf-8')
     return bcrypt.hashpw(password, bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_pass = _hash_password(password)
+            user = self._db.add_user(email=email, hashed_password=hashed_pass)
+            return user
+        if user:
+            raise ValueError("User {} already exists".format(email))
